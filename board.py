617cf039e72c33f08c444dd064a59542bc389538
srajput0 <pshycho566@gmail.com>
2025-07-22 16:26:41 +0530

    Update board.py

--- board.py
+++ board.py
@@ -1,1 +1,6 @@
+class Board:
+    def __init__(self):
+        self.finish_line = 50  # Simplified board finish
 
+    def check_winner(self, player):
+        return player.position >= self.finish_line
