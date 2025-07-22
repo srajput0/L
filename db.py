8e23c8012d1005b3da8fe5edf5d95814b52d8312
srajput0 <pshycho566@gmail.com>
2025-07-22 16:30:50 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,15 @@
+from pymongo import MongoClient
 
+# Replace this URI with your MongoDB connection string
+client = MongoClient("mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority")
+db = client["ludo_game"]
+collection = db["games"]
+
+def save_game(chat_id, data):
+    collection.update_one({"chat_id": chat_id}, {"$set": data}, upsert=True)
+
+def load_game(chat_id):
+    return collection.find_one({"chat_id": chat_id})
+
+def delete_game(chat_id):
+    collection.delete_one({"chat_id": chat_id})
