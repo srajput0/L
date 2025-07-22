23e138e8a00b3c0601f685efc690485f5eeaea53
srajput0 <pshycho566@gmail.com>
2025-07-22 16:33:09 +0530

    Update player.py

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, position=0):
         self.name = name
-        self.position = 0
+        self.position = position
 
     def move(self, steps):
         self.position += steps
